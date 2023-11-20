04279396342c52e5470035f5a81e35eabdb8aa66
DragonheartLX <dragonheartlx@outlook.com>
2023-11-20 21:08:01 +0800

    Day 3

--- Leetcode/7.整数反转.cpp
+++ Leetcode/7.整数反转.cpp
@@ -3,12 +3,36 @@
  *
  * [7] 整数反转
  */
+#include <iostream>
 
+using namespace std;
 // @lc code=start
 class Solution {
 public:
     int reverse(int x) {
+        // 反转操作简单,难点在于溢出判断
+        
+        int out = 0;
 
+        while (x)
+        {
+            int tmp = x % 10;
+            x /= 10;
+
+            /*
+                INT_MIN <= out * 10 + tmp <= INT_MAX
+                INT_MIN / 10 <= out + tmp / 10 <= INT_MAX / 10
+                (INT_MIN - tmp) / 10 <= out <= (INT_MAX - tmp) / 10
+                INT_MIN / 10 <= out <= INT_MAX / 10 - tmp / 10
+            */
+
+            if (INT_MIN / 10 <= out && out <= INT_MAX / 10 - tmp / 10)
+                out = out * 10 + tmp;
+            else
+                return 0;
+        }
+        
+        return out;
     }
 };
 // @lc code=end
